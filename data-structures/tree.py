8fc3f5ea38583affe04a9f64e37feb57e04199c0
sulaimon <sulele04@gmail.com>
2026-06-28 19:42:12 +0100

    -recursion basics

--- data-structures/tree.py
+++ data-structures/tree.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 class Tree:
     def __init__(self, val):
         self.val = val
@@ -14,12 +16,51 @@ intltr.right = Tree(3)
 intrtr.left = Tree(6)
 intrtr.right = Tree(7)
 
-def dfs(tree):
+## pre-order traversal
+def pdfs(tree):
     if tree is None:
         return
     print(tree.val)
-    dfs(tree.left)
-    dfs(tree.right)
+    pdfs(tree.left)
+    pdfs(tree.right)
 
-dfs(root)
+pdfs(root)
+print()
+
+## in-order traversal
+def idfs(tree):
+    if tree is None:
+        return
+    idfs(tree.left)
+    print(tree.val)
+    idfs(tree.right)
+
+idfs(root)
+print()
+
+## post-order traversal
+def podfs(tree):
+    if tree is None:
+        return
+    podfs(tree.left)
+    podfs(tree.right)
+    print(tree.val)
+
+podfs(root)
+print()
+
+## bread-first search
+def bfs(tree):
+    if tree is None:
+        return
+    queue = deque([tree])
+    while queue:
+        node = queue.popleft()
+        print(node.val)
+        if node.left:
+            queue.append(node.left)
+        if node.right:
+            queue.append(node.right)
+
+bfs(root)
 
